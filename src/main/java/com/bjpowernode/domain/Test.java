973fbb68363aa37adfd3b78bde9b7d3ca6771bae
zs <zs@bjpowernode.com>
2021-03-01 18:56:41 +0800

    123

--- src/main/java/com/bjpowernode/domain/Test.java
+++ src/main/java/com/bjpowernode/domain/Test.java
@@ -1,11 +1,13 @@
 package com.bjpowernode.domain;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Test {
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>(31);
-
+        HashSet<Object> set = new HashSet<>();
+        set.add(1);
     }
 
 }
