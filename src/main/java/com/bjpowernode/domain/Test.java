8805346207b9d5262d358944f5b5f7c3d583d6a9
zs <zs@bjpowernode.com>
2021-03-01 21:03:26 +0800

    123

--- src/main/java/com/bjpowernode/domain/Test.java
+++ src/main/java/com/bjpowernode/domain/Test.java
@@ -1,13 +1,20 @@
 package com.bjpowernode.domain;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Vector;
 
 public class Test {
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>(31);
         HashSet<Object> set = new HashSet<>();
         set.add(1);
+
+        ArrayList<Object> arrayList = new ArrayList<>();
+        Vector<Object> vector = new Vector<>();
+
+
     }
 
 }
